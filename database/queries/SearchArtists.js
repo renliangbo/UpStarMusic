bc4c8a4bc20ce8fac93a62c2553cdc39f25f057f
renliangbo <renna0508@126.com>
2017-12-23 00:02:39 +0800

    search

--- database/queries/SearchArtists.js
+++ database/queries/SearchArtists.js
@@ -10,8 +10,8 @@ const {Artist} = require('../models/artist');
  * @return {promise} A promise that resolves with the artists, count, offset, and limit
  */
 module.exports = (criteria, sortProperty, offset = 0, limit = 10) => {
-
-	const query=Artist.find({})
+	console.log(criteria)
+	const query=Artist.find(buildQuery(criteria))
 	.sort({[sortProperty]:1})
 	.skip(offset)
 	.limit(limit);
@@ -26,3 +26,27 @@ module.exports = (criteria, sortProperty, offset = 0, limit = 10) => {
 		}
 	})	
 };
+
+const buildQuery=(criteria)=>{
+	const query={};
+	if(criteria.name){
+		query.$text={
+				$search:criteria.name
+			}
+		}
+	
+	if(criteria.age){
+		query.age={
+			$gte:criteria.age.min,
+			$lte:criteria.age.max
+		}
+	}
+	if(criteria.yearsActive){
+		query.yearsActive={
+			$gte:criteria.yearsActive.min,
+			$lte:criteria.yearsActive.max
+		}
+
+	}
+	return query
+}
